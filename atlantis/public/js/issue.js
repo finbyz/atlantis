ef5768c1aa67842dae4e5bd62ba33e93e434e683
finbyz <info@finbyz.com>
2023-01-12 13:18:01 +0530

    doctype push

--- atlantis/public/js/issue.js
+++ atlantis/public/js/issue.js
@@ -1,13 +1,13 @@
-console.log("chale chhe pan")
-frappe.ui.form.on("Issue", {
-	refresh: function(frm) {
-        if(frm.doc.__islocal){
-            frm.set_value('agent_name' , frappe.session.user)
-        }   
-    },
-    validate:function(frm){
-         let phoneno = frm.doc.caller_contact_number
+// console.log("chale chhe pan")
+// frappe.ui.form.on("Issue", {
+// 	refresh: function(frm) {
+//         if(frm.doc.__islocal){
+//             frm.set_value('agent_name' , frappe.session.user)
+//         }   
+//     },
+//     validate:function(frm){
+//          let phoneno = frm.doc.caller_contact_number
          
-    }
-});
+//     }
+// });
 
